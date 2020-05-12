57c99e5c94fea0e3b4400e7d918056eda1f945d5
slopes <stephane@renovationman.com>
2020-05-12 11:57:31 +0200

    1. Intro to Design Pattern : setting behavior dynamically

--- src/main/java/fr/slopesneves/hfdp/duck/MiniDuckSimulator.java
+++ src/main/java/fr/slopesneves/hfdp/duck/MiniDuckSimulator.java
@@ -1,9 +1,16 @@
 package fr.slopesneves.hfdp.duck;
 
+import fr.slopesneves.hfdp.duck.behavior.fly.FlyRocketPowered;
+
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
