caa29b0fa6ccad913e0c7254c00c865a2bef0a58
slopes <stephane@renovationman.com>
2020-05-13 11:45:51 +0200

    2. Reworking the Weather Station with the built-in support

--- src/main/java/fr/slopesneves/hfdp/weatherstation/Display.java
+++ src/main/java/fr/slopesneves/hfdp/weatherstation/Display.java
@@ -1,5 +1,13 @@
 package fr.slopesneves.hfdp.weatherstation;
 
+import java.util.Observable;
+import java.util.Observer;
+
 public interface Display extends Observer {
+    @Override
+    default void update(Observable o, Object arg) {
+        update();
+    }
+    void update();
     void display();
 }
