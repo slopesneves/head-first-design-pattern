e69d79edfe2b58c789d88e9c5da4e3f50b38be11
slopes <stephane@renovationman.com>
2020-05-19 17:38:54 +0200

    6. Testing the ceiling fan

--- src/main/java/fr/slopesneves/hfdp/command/RemoteLoader.java
+++ src/main/java/fr/slopesneves/hfdp/command/RemoteLoader.java
@@ -1,7 +1,6 @@
 package fr.slopesneves.hfdp.command;
 
-import static fr.slopesneves.hfdp.command.RemoteControl.RemoteControlButtonIndex.ONE;
-import static fr.slopesneves.hfdp.command.RemoteControl.RemoteControlButtonIndex.TWO;
+import static fr.slopesneves.hfdp.command.RemoteControl.RemoteControlButtonIndex.*;
 
 public class RemoteLoader {
     public static void main(String[] args) {
@@ -15,12 +14,28 @@ public class RemoteLoader {
         StereoOffCommand stereoOffCommand = new StereoOffCommand(stereo);
         remote.setCommand(ONE, lightOnCommand, lightOffCommand);
         remote.setCommand(TWO, stereoOnCommand, stereoOffCommand);
-        System.out.println(remote);
 
         remote.onButtonPushed(ONE);
         remote.offButtonPushed(ONE);
         remote.onButtonPushed(TWO);
         remote.offButtonPushed(TWO);
         remote.undo();
+
+        CeilingFan fan = new CeilingFan();
+        CeilingFanOffCommand fanOffCommand = new CeilingFanOffCommand(fan);
+        remote.setCommand(THREE, new CeilingFanHighCommand(fan), fanOffCommand);
+        remote.setCommand(FOUR, new CeilingFanMediumCommand(fan), fanOffCommand);
+        remote.setCommand(FIVE, new CeilingFanLowCommand(fan), fanOffCommand);
+
+        remote.onButtonPushed(THREE);
+        remote.onButtonPushed(FOUR);
+        remote.undo();
+        remote.onButtonPushed(FIVE);
+        remote.undo();
+        remote.onButtonPushed(FIVE);
+        remote.onButtonPushed(THREE);
+        remote.undo();
+
+        System.out.println(remote);
     }
 }
